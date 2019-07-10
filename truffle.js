9a4e2a640928915b5fb350f21bd40c07136b6e31
Greg Kapka <greg@oraclize.it>
2019-07-10 18:35:58 +0100

    fix(config): adds explicit compiler version to the truffle config

--- truffle.js
+++ truffle.js
@@ -5,5 +5,16 @@ module.exports = {
       port: 8545,
       network_id: "*"
     }
+  },
+  compilers: {
+    solc: {
+      version: "0.5.0",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+        }
+      }
+    }
   }
 }
