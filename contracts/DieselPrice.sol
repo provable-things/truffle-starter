f766b36266b9fc99daf05079916e1ce639b772e3
Greg Kapka <greg@oraclize.it>
2019-07-10 15:13:14 +0100

    feat(contract): <- rebrand & refactor that

--- contracts/DieselPrice.sol
+++ contracts/DieselPrice.sol
@@ -1,27 +1,33 @@
 pragma solidity ^0.5.0;
 
-import "./oraclize/oraclizeAPI_0.5.sol";
+import "./provable/provableAPI_0.5.sol";
 
-contract DieselPrice is usingOraclize {
+contract DieselPrice is usingProvable {
 
     uint public dieselPriceUSD;
 
-    event LogNewDieselPrice(string price);
-    event LogNewOraclizeQuery(string description);
+    event LogNewDieselPrice(string _price);
+    event LogNewProvableQuery(string _description);
 
-    constructor() public {
-        update(); // First check at contract creation...
+    constructor()
+        public
+    {
+        fetchDieselPriveViaProvable();
     }
 
-    function __callback(bytes32 myid, string memory result) public {
-        require(msg.sender == oraclize_cbAddress());
-        emit LogNewDieselPrice(result);
-        dieselPriceUSD = parseInt(result, 2); // Let's save it as cents...
-        // Now do something with the USD Diesel price...
+    function __callback(bytes32 _queryID, string memory _result)
+        public
+    {
+        require(msg.sender == provable_cbAddress());
+        emit LogNewDieselPrice(_result);
+        dieselPriceUSD = parseInt(_result, 2); // Let's save it as cents...
     }
 
-    function update() public payable {
-        emit LogNewOraclizeQuery("Oraclize query was sent, standing by for the answer...");
-        oraclize_query("URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
+    function fetchDieselPriveViaProvable()
+        public
+        payable
+    {
+        emit LogNewProvableQuery("Provable query was sent, standing by for the answer...");
+        provable_query("URL", "xml(https://www.fueleconomy.gov/ws/rest/fuelprices).fuelPrices.diesel");
     }
 }
