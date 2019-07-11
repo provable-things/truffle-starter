8f4ae87714f6509d88af88400b0e709067904124
Greg Kapka <greg@oraclize.it>
2019-07-11 18:07:12 +0100

    chore(contract): reorder fxns to more sensible arrangement

--- contracts/EthPrice.sol
+++ contracts/EthPrice.sol
@@ -15,14 +15,6 @@ contract EthPrice is usingProvable {
         fetchEthPriveViaProvable();
     }
 
-    function __callback(bytes32 _queryID, string memory _result)
-        public
-    {
-        require(msg.sender == provable_cbAddress());
-        ethPriceCents = parseInt(_result, 2);
-        emit LogNewEthPrice(ethPriceCents);
-    }
-
     function fetchEthPriveViaProvable()
         public
         payable
@@ -33,4 +25,13 @@ contract EthPrice is usingProvable {
             "json(https://api.kraken.com/0/public/Ticker?pair=ETHUSD).result.XETHZUSD.c.0"
         );
     }
+
+    function __callback(bytes32 _queryID, string memory _result)
+        public
+    {
+        require(msg.sender == provable_cbAddress());
+        ethPriceCents = parseInt(_result, 2);
+        emit LogNewEthPrice(ethPriceCents);
+    }
+
 }
