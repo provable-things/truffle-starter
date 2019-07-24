cee421df7d7c4b1ed7eb46bed7f97adc1ace5b0d
Greg Kapka <greg@oraclize.it>
2019-07-24 11:44:44 +0100

    fix(utils): flatten out dir ∵ only one util now

--- test/util/Events.js
+++
@@ -1,7 +0,0 @@
-module.exports.waitForEvent = (event, from = 0, to = 'latest') =>
-  new Promise (
-      (resolve,reject) => event(
-          {fromBlock: from, toBlock: to},
-          (e, ev) => e ? reject(e) : resolve(ev)
-      )
-  );
